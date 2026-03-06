54dc1cf8a6b7c0838700b2d3addab54b4c8e2292
Niharikatn <niharikanaidut@gmail.com>
2026-03-06 19:44:02 +0530

    sal

--- backend/models/Employee.js
+++ backend/models/Employee.js
@@ -4,7 +4,8 @@ const employeeSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
    password: { type: String, required: true },
-  position: { type: String, required: true }
+  position: { type: String, required: true },
+  salary: { type: Number, required: true }
 });
 
 module.exports = mongoose.model("Employee", employeeSchema);
